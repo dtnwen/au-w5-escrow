339c1f51541817d6b84b631cc3b28c02c5fa4de2
dt <ndneiht1200.relax@gmail.com>
2023-04-03 13:17:24 +0300

    commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config()
+
+const {API_URL, PRIVATE_KEY} = process.env;
 
 module.exports = {
+  defaultNetwork: "sepolia",
+  networks: {
+    sepolia: {
+      url: API_URL,
+      accounts: [PRIVATE_KEY]
+    }
+  },
   solidity: "0.8.17",
   paths: {
     artifacts: "./app/src/artifacts",
